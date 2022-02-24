92add810baee8c39222e0a9d03760ae96496713f
Ramon Rasheed <ramonrash2@gmail.com>
2022-02-24 21:00:18 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pheity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "Poly Ilaro ";
+
+            Console.Write("Enter Second Integer: ");
+            int new_int = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Double: ");
+            double new_double = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Second String: ");
+            string new_string = Console.ReadLine();
+
+            Console.WriteLine(i + new_int);
+            Console.WriteLine(d + new_double);
+            Console.WriteLine(s + new_string);
+
+        }
+    }
+}
